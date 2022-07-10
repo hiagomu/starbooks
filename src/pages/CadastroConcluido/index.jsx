cb2541ab5dba65373e5ef35afd58ca0d3e412ea9
Hiago Murilo <hiagomuuu@gmail.com>
2022-07-09 21:06:46 -0300

    Commit 10: Alteração na Página de Cadastro Concluído

--- src/pages/CadastroConcluido/index.jsx
+++ src/pages/CadastroConcluido/index.jsx
@@ -5,7 +5,7 @@ import '../CadastroConcluido/styles.css'
 export default function CadastroConcluido() {
     return(
         <main className="container-concluido">
-            <h2>Cadastrado com sucesso!</h2>
+            <h2>Cadastro concluído!</h2>
             <img src={img} alt="Cadastro concluído"/>
         </main>
     );
